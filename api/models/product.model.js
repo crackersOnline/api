c698ef40b2de99217eff56b13d6e8db8caa1984b
mathar beevi <mathar.beevi@nbcuni.com>
2020-05-30 02:04:24 +0530

    column name changed

--- api/models/product.model.js
+++ api/models/product.model.js
@@ -1,7 +1,7 @@
 const sql = require('./db.js')
 
 const getProducts = (result) => {
-    var dbQuery = "select productID, product.categoryID as categoryID, categoryName, productName, productMRP, prouductPrice, productDescription, Product.status as productStatus, 0 as count from product inner join category on Category.categoryID=product.categoryID AND Category.status =1 where Product.status = 1 ORDER BY Category.categoryID ASC"
+    var dbQuery = "select productID, product.categoryID as categoryID, categoryName, productName, productMRP, productPrice, productDescription, Product.status as productStatus, 0 as count from product inner join category on Category.categoryID=product.categoryID AND Category.status =1 where Product.status = 1 ORDER BY Category.categoryID ASC"
     console.log("getProducts",dbQuery);
     sql.query(dbQuery, (err, res) => {
         if(err){
